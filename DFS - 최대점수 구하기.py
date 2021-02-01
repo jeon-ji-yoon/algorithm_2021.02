abc5e4abdf46d5ec154858e2404b4cc1e54a4497
jeon-ji-yoon <appleru1515@gmail.com>
2021-02-01 17:35:22 +0900

    0201_ DFS최대점수 구하기_모범답안

--- DFS - 최대점수 구하기.py
+++ DFS - 최대점수 구하기.py
@@ -2,12 +2,14 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 def DFS(L, sum1, sum2):
+    global res
     if(L == N):
-        if( sum2 <=M):
-            res.append(sum1)
+        if( sum2 > M):
+            return
+        if(sum1 > res):
+            res = sum1
 
     else:
-        #for j in range(N): 0 1 2 3 4...
         #DFS의 가지가 두개(점수를 포함하는지 아닌지)로만 뻗으므로 for문을 쓸 필요 없음
         DFS(L+1, sum1 + s[L], sum2 + t[L])
         DFS(L+1, sum1, sum2)
@@ -18,7 +20,7 @@ if __name__ == "__main__":
     N, M=map(int, input().split())
     s=[] #리스트에 N개의 자료가 들어있다
     t=[]
-    res = []
+    res = -2147000000
 
     for i in range(N):
         a, b = map(int, input().split())
@@ -26,4 +28,4 @@ if __name__ == "__main__":
         t.append(b)
 
     DFS(0,0,0)
-    print(max(res))
+    print(res)
