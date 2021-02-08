624983094a138bc5a4559f4ce8b054f29f7e6667
jeon-ji-yoon <appleru1515@gmail.com>
2021-02-08 21:28:29 +0900

    0208

---
+++ dp - 최대로 긴 부분증가수열 찾기.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+if __name__ == "__main__":
+    n = int(input())
+    v = list(map(int, input().split() ))
+
+    #5가 맨 마지막 요소인 증가수열의 갯수 : 1
+    #3이 맨 마지막 요소인 증가수열의 갯수 : 1
+    v.insert(0,0)
+    res = [0]*(n+1)
+    res[1] = 1
+    res[2] = 1
+
+    for i in range(3, n+1):
+        temp = list()
+        for j in range(1,i):
+            if v[j] < v[i]:
+                temp.append(res[j])
+        if(len(temp)!=0):
+            res[i] = max(temp)+1
+
+    print(max(res))
