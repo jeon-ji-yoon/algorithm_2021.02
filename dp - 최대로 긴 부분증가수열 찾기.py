35b77f9af784a54dd5e151d24eb014a6be0a9cc6
jeon-ji-yoon <appleru1515@gmail.com>
2021-02-09 17:50:50 +0900

    0209

--- dp - 최대로 긴 부분증가수열 찾기.py
+++ dp - 최대로 긴 부분증가수열 찾기.py
@@ -10,14 +10,15 @@ if __name__ == "__main__":
     v.insert(0,0)
     res = [0]*(n+1)
     res[1] = 1
-    res[2] = 1
 
-    for i in range(3, n+1):
+    for i in range(2, n+1):
         temp = list()
         for j in range(1,i):
             if v[j] < v[i]:
                 temp.append(res[j])
         if(len(temp)!=0):
             res[i] = max(temp)+1
+        else:
+            res[i] +=1
 
     print(max(res))
