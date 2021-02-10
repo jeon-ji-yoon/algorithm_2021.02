f7b9be219062f2432e465fb46ca6354f32ee46a2
jeon-ji-yoon <appleru1515@gmail.com>
2021-02-10 20:17:24 +0900

    0210

--- dp - 최대한으로 선 연결하기.py
+++ dp - 최대한으로 선 연결하기.py
@@ -4,18 +4,19 @@ sys.stdin = open("input.txt", "r")
 if __name__ == "__main__":
     n = int(input())
     v = list(map(int, input().split() ))
+
     v.insert(0,0)
-    res = [0]*(n+1)
-    res[1] = 1
+    dy = [0]*(n+1)
+    dy[1] = 1
 
     for i in range(2, n+1):
         temp = list()
         for j in range(1,i):
             if v[j] < v[i]:
-                temp.append(res[j])
+                temp.append(dy[j])
         if(len(temp)!=0):
-            res[i] = max(temp)+1
+            dy[i] = max(temp)+1
         else:
-            res[i] +=1
+            dy[i] +=1
 
-    print(max(res))
+    print(max(dy))
